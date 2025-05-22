9d074b566da8088aa122d5b1e49e0b5a2aee0d40
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-22 16:50:16 +0000

    test

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+
+  const radius = params.get("radius")
+  console.log(radius)
+
+  // process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+  const lengthOfRadius =
+    "<ul>\n<li>Radius = " 
+    + radius + 
+    "</li>\n</ul>"
+
+  // output
+  document.getElementById("side").innerHTML = lengthOfRadius
+  document.getElementById("volume").innerHTML = "Volume is: " + volume + " mm²"
+}
